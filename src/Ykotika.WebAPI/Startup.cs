1536f92a0ae9ef3dae7886d3b6a54210275a84db
TIKI-developer <117838226+TIKI-developer@users.noreply.github.com>
2024-11-23 21:31:36 +0800

    Update Startup.cs

--- src/Ykotika.WebAPI/Startup.cs
+++ src/Ykotika.WebAPI/Startup.cs
@@ -49,7 +49,7 @@ namespace Ykotika.WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-            if (env.IsProduction() || env.IsProduction()) 
+            if (env.IsProduction() || env.IsStaging()) 
             {
                 app.UseCustomExceptionHandler();
             }
