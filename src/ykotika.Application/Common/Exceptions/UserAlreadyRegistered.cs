5c60311a3b5343ed3d86b34fb68e1db3397f57cd
TIKI-developer <117838226+TIKI-developer@users.noreply.github.com>
2024-11-23 19:18:34 +0800

    Revert "Fix folder duplication"
    This reverts commit be38a33539f9aa7b79f2977985c2b91dfa05bcb6.

---
+++ src/ykotika.Application/Common/Exceptions/UserAlreadyRegistered.cs
@@ -0,0 +1,5 @@
+﻿namespace Ykotika.Application.Common.Exceptions
+{
+    public class UserAlreadyRegistered(string email) 
+        : Exception($"User with email: \"{email}\" already registered.") { }
+}
---
+++ src/ykotika.Domain/DefaultUserModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Ykotika.Domain
+{
+    public class DefaultUserModel : UserModel
+    {
+        public override UserRole Role => UserRole.Default;
+    }
+}
