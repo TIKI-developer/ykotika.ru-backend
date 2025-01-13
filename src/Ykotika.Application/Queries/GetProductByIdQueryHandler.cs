b4a9cd0e7c2d0d0ec0a185eeddcb8ee9f3c7ca31
TIKI-developer <117838226+TIKI-developer@users.noreply.github.com>
2025-01-13 19:53:03 +0800

    Update GetProductByIdQueryHandler.cs

--- src/Ykotika.Application/Queries/GetProductByIdQueryHandler.cs
+++ src/Ykotika.Application/Queries/GetProductByIdQueryHandler.cs
@@ -29,6 +29,7 @@ namespace Ykotika.Application.Queries
                 .Include(e => e.ProductType)
                 .Include(e => e.FormRecord)
                 .ThenInclude(e => e.Form)
+                .Include(e => e.Source)
                 .FirstOrDefaultAsync(e => e.Id == request.Id, cancellationToken)
                 ?? throw new NotFoundException(nameof(Product), request.Id);
 
