eca869e3803f0b45ad2f53643533331735e4dd5a
TIKI-developer <117838226+TIKI-developer@users.noreply.github.com>
2025-01-16 01:00:15 +0800

    Update BaseGetListQueryHandler.cs

--- src/Ykotika.Application/Queries/BaseGetListQueryHandler.cs
+++ src/Ykotika.Application/Queries/BaseGetListQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 using Ykotika.Application.Extensions;
 using Ykotika.Application.Interfaces;
 
@@ -10,14 +11,25 @@ namespace Ykotika.Application.Queries
         IMapper mapper)
         : BaseGetQueryHandler(dbContext, mapper)
     {
-        protected static IQueryable<T> Sort<T>(IQueryable<T> query, string? sortBy, bool desc = false)
+        protected static IQueryable<T> Sort<T>(IQueryable<T> query, string? sortBy = "Timestamps_CreatedAt", bool desc = false)
         {
             if (!string.IsNullOrEmpty(sortBy))
             {
-                query = desc
-                ? query.OrderByDescending
-                    (c => EF.Property<object>(c, sortBy.ToLower().FirstCharToUpper()))
-                    : query.OrderBy(c => EF.Property<object>(c, sortBy.ToLower().FirstCharToUpper()));
+                var parameter = Expression.Parameter(typeof(T), "c");
+                var propertyNames = sortBy.Split('_');
+                Expression property = parameter;
+
+                foreach (var propertyName in propertyNames)
+                {
+                    property = Expression.PropertyOrField(property, propertyName.ToLower().FirstCharToUpper());
+                }
+
+                var lambda = Expression.Lambda<Func<T, object>>(Expression.Convert(property, typeof(object)), parameter);
+                query = desc ? query.OrderByDescending(lambda) : query.OrderBy(lambda);
+            }
+            else
+            {
+                query = desc ? query.OrderByDescending(c => EF.Property<object>(EF.Property<object>(c, "Timestamps"), "CreatedAt")) : query.OrderBy(c => EF.Property<object>(EF.Property<object>(c, "Timestamps"), "CreatedAt"));
             }
 
             return query;
