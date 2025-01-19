f42c609e5cb6be801cfaa6bf80cccc0ac5df2858
TIKI-developer <117838226+TIKI-developer@users.noreply.github.com>
2025-01-19 23:42:30 +0800

    Update DbInitializer.cs

--- src/Ykotika.Persistence/DbInitializer.cs
+++ src/Ykotika.Persistence/DbInitializer.cs
@@ -10,7 +10,7 @@ namespace Ykotika.Persistence
 
         public void Initialize(YkotikaDbContext restaurantDbContext)
         {
-            //restaurantDbContext.Database.EnsureDeleted();
+            restaurantDbContext.Database.EnsureDeleted();
             restaurantDbContext.Database.EnsureCreated();
 
             CreateSuperUser(restaurantDbContext);
@@ -27,7 +27,7 @@ namespace Ykotika.Persistence
                     Name = "admin",
                     PasswordHash = _passwordHasher.Generate("qwerty123"),
                     Timestamps = new Timestamps(),
-                    Roles = new List<UserRole> { UserRole.Admin, UserRole.Verified, UserRole.Moderator, UserRole.Director },
+                    Roles = [UserRole.Admin, UserRole.Verified, UserRole.Moderator, UserRole.Director],
                     ConfirmedPersonalDataProcessingPolicy = true
                 };
                 context.Users.Add(superUser);
