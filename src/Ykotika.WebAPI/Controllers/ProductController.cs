a0768f7b0be4c2a7828bd60ec524446ee7b26d92
Сашка <sasha.bannov.2004@mail.ru>
2025-02-14 11:59:57 +0800

    Update ProductController.cs

--- src/Ykotika.WebAPI/Controllers/ProductController.cs
+++ src/Ykotika.WebAPI/Controllers/ProductController.cs
@@ -271,9 +271,9 @@ namespace Ykotika.WebAPI.Controllers
         public void Mapping(Profile profile)
         {
             profile.CreateMap<ProductFilterQueryParams, ProductFilterDto>()
-                .ForMember(to => to.IsPublished,
-                    opt => opt.MapFrom(from => Enum.Parse<ProductStatus>(from.Status)))
                 .ForMember(to => to.Status,
+                    opt => opt.MapFrom(from => Enum.Parse<ProductStatus>(from.Status)))
+                .ForMember(to => to.IsPublished,
                     opt => opt.MapFrom(from =>
                         string.IsNullOrEmpty(from.IsPublished) ? (bool?)null :
                         (from.IsPublished.Equals("true", StringComparison.OrdinalIgnoreCase) ? (bool?)true : (bool?)false)))
