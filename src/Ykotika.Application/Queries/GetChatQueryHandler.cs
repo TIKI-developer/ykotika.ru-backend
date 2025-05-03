11730420eaa1c585825481d20ebbd23f86c9ba34
TIKI-developer <117838226+TIKI-developer@users.noreply.github.com>
2025-05-03 23:59:33 +0800

    Update GetChatQueryHandler.cs

--- src/Ykotika.Application/Queries/GetChatQueryHandler.cs
+++ src/Ykotika.Application/Queries/GetChatQueryHandler.cs
@@ -20,6 +20,7 @@ namespace Ykotika.Application.Queries
                 .Chats
                 .Include(e => e.Members)
                 .Include(e => e.Messages)
+                .ThenInclude(e => e.Attachments)
                 .AsNoTracking()
                 .FirstOrDefaultAsync(e => e.Id == request.Id, cancellationToken)
                 ?? throw new NotFoundException(nameof(Chat), cancellationToken);
